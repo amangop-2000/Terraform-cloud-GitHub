7c6545ef63239b7730ed9201f05ccf25cc2d677d
amangop-2000 <amangopkv@gmail.com>
2026-07-06 09:25:53 +0530

    tag creation by aman

--- main.tf
+++ main.tf
@@ -16,4 +16,7 @@ provider "aws" {
 resource "aws_instance" "aman_prod" {
 	ami = "ami-0b6d9d3d33ba97d99" 
 	instance_type = "t3.micro"
+        tags = {
+                name = "hemanth(GOAT)_server"
+        }
 }
